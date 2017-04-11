3e6a85a41929180c074325075038c3d195f3a612
Niveditha Kalavakonda <nkalavakonda@gmail.com>
2017-04-11 10:47:19 -0700

    Update buildMesh

--- Assets/buildMesh.cs
+++ Assets/buildMesh.cs
@@ -14,32 +14,32 @@ public class buildMesh : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+        //Mesh Filter
 		MeshFilter mf = GetComponent<MeshFilter>();
-		Mesh mesh = mf.mesh;
+		Mesh mesh = mf.mesh; 
 
-		//Vertices//
+		//Vertices
 		Vector3[] vertices = new Vector3[]
 		{
-			//front face//
+			//front face
 			vertLeftTopFront,//left top front, 0
 			vertRightTopFront,//right top front, 1
 			new Vector3(-1,-1,1),//left bottom front, 2
 			new Vector3(1,-1,1),//right bottom front, 3
 
-			//back face//
+			//rear face
 			vertRightTopBack,//right top back, 4
 			vertLeftTopBack,//left top back, 5
 			new Vector3(1,-1,-1),//right bottom back, 6
 			new Vector3(-1,-1,-1),//left bottom back, 7
 
-			//left face//
+			//left face
 			vertLeftTopBack,//left top back, 8
 			vertLeftTopFront,//left top front, 9
 			new Vector3(-1,-1,-1),//left bottom back, 10
 			new Vector3(-1,-1,1),//left bottom front, 11
 
-			//right face//
+			//right face
 			vertRightTopFront,//right top front, 12
 			vertRightTopBack,//right top back, 13
 			new Vector3(1,-1,1),//right bottom front, 14
@@ -59,38 +59,39 @@ public class buildMesh : MonoBehaviour {
 
 		};
 
-		//Triangles// 3 points, clockwise determines which side is visible
-		int[] triangles = new int[]
+		//Triangles// 3 points, clockwise => visible
+		int[] triangles = new int[] //Array of integers
 		{
-			//front face//
+			//front face
 			0,2,3,//first triangle
 			3,1,0,//second triangle
 
-			//back face//
+			//back face
 			4,6,7,//first triangle
 			7,5,4,//second triangle
 
-			//left face//
+			//left face
 			8,10,11,//first triangle
 			11,9,8,//second triangle
 
-			//right face//
+			//right face
 			12,14,15,//first triangle
 			15,13,12,//second triangle
 
-			//top face//
+			//top face
 			16,18,19,//first triangle
 			19,17,16,//second triangle
 
-			//bottom face//
+			//bottom face
 			20,22,23,//first triangle
 			23,21,20//second triangle
 		};
 
-		//UVs//
+		//When textures are includes, we have UVs
 		Vector2[] uvs = new Vector2[]
 		{
-			//front face// 0,0 is bottom left, 1,1 is top right//
+			//front face
+            // 0,0 is bottom left, 1,1 is top right
 			new Vector2(0,1),
 			new Vector2(0,0),
 			new Vector2(1,1),
@@ -122,10 +123,11 @@ public class buildMesh : MonoBehaviour {
 			new Vector2(1,0)
 		};
 
-		mesh.Clear ();
-		mesh.vertices = vertices;
+		mesh.Clear (); //Clears the mesh information
+		mesh.vertices = vertices; 
 		mesh.triangles = triangles;
 		mesh.uv = uvs;
+        //mesh.Optimize();
 		;
 		mesh.RecalculateNormals();
 	
@@ -148,7 +150,7 @@ public class buildMesh : MonoBehaviour {
 
 		}
 
-		//morph to cube//
+		//morph to cube
 		if(shapeN == 0)
 		{
 			vertLeftTopFront = Vector3.Lerp(vertLeftTopFront, new Vector3(-1,1,1),Time.deltaTime);
@@ -157,7 +159,7 @@ public class buildMesh : MonoBehaviour {
 			vertLeftTopBack = Vector3.Lerp(vertLeftTopBack, new Vector3(-1,1,-1),Time.deltaTime);
 		}
 
-		//morph to pyramid//
+		//morph to pyramid
 		if(shapeN == 1)
 		{
 			vertLeftTopFront = Vector3.Lerp(vertLeftTopFront, new Vector3(0,1,0),Time.deltaTime);
@@ -165,8 +167,8 @@ public class buildMesh : MonoBehaviour {
 			vertRightTopBack = Vector3.Lerp(vertRightTopBack, new Vector3(0,1,0),Time.deltaTime);
 			vertLeftTopBack = Vector3.Lerp(vertLeftTopBack, new Vector3(0,1,0),Time.deltaTime);
 		}
-
-		//morph to ramp//
+    
+		//morph to ramp
 		if(shapeN == 2)
 		{
 			vertLeftTopFront = Vector3.Lerp(vertLeftTopFront, new Vector3(-1,-1,2),Time.deltaTime);
@@ -175,7 +177,7 @@ public class buildMesh : MonoBehaviour {
 			vertLeftTopBack = Vector3.Lerp(vertLeftTopBack, new Vector3(-1,0.5f,-1),Time.deltaTime);
 		}
 
-		//morph to roof//
+		//morph to roof
 		if(shapeN == 3)
 		{
 			vertLeftTopFront = Vector3.Lerp(vertLeftTopFront, new Vector3(-1,0.2f,0),Time.deltaTime);
